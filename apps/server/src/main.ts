102a6176ca5d32903da161cc948263e41742256c
arturvoloshyn <arturvoloshyn@gmail.com>
2024-02-01 22:25:49 +0200

    refactor(server): format the code

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -15,9 +15,7 @@ async function bootstrap() {
 
   app.use(cookieParser());
   app.enableCors({
-    origin: [
-      'http://localhost:3001',
-    ],
+    origin: ['http://localhost:3001'],
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe());
